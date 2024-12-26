71a2bb1a7f7f84b6e25a430119d6c394607a8727
manjugh <manjunath.gh@ondosystems.com>
2024-12-26 11:39:04 +0530

    Upgraded to: Java 21 Spring boot - 3.4.1 Spring cloud confg - 2024.0.1

--- currency-exchange/src/main/java/com/example/learning/controller/CurrencyExchange.java
+++ currency-exchange/src/main/java/com/example/learning/controller/CurrencyExchange.java
@@ -1,8 +1,8 @@
 package com.example.learning.controller;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import java.math.BigDecimal;
 
 @Entity
